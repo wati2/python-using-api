bdde3ef02716b6bedfbdb6e6352920c15f849583
wati2 <yt.wati@gmail.com>
2020-06-07 07:49:51 +0900

    테스트 정상작동

--- coupang/_coupang_test.py
+++ coupang/_coupang_test.py
@@ -7,38 +7,31 @@ import ssl
 # venderId, accesskey, secretkey 파일(memo.py)로 따로 관리
 import memo
 
+# 테스트 완료 2020-06-07
+
 os.environ['TZ'] = 'GMT+0'
 
 datetime=time.strftime('%y%m%d')+'T'+time.strftime('%H%M%S')+'Z'
 method = "GET"
-vernderId = memo.vernderId
-sellerProductId = memo.sellerProductId
-#replace with your own vendorId
-# path = f"/v2/providers/openapi/apis/api/v4/vendors/{vernderId}/returnRequests"
-path = f"/v2/providers/seller_api/apis/api/v1/marketplace/seller-products/{sellerProductId}"
-# query = urllib.parse.urlencode({"createdAtFrom": "2020-06-04", "createdAtTo": "2020-06-05", "status": "UC"})
 
-message = datetime+method+path# +query
-
-#replace with your own accesskey
+vendorId = memo.vendorId
 accesskey = memo.accesskey
-#replace with your own secretKey
 secretkey = memo.secretkey
 
-#authorize, demonstrate how to generate hmac signature here
+path = f"/v2/providers/openapi/apis/api/v4/vendors/{vendorId}/returnRequests"
+query = urllib.parse.urlencode({"createdAtFrom": "2020-06-04", "createdAtTo": "2020-06-05", "status": "UC"})
+
+message = datetime+method+path+query
+
 signature=hmac.new(secretkey.encode('utf-8'),message.encode('utf-8'),hashlib.sha256).hexdigest()
 authorization  = "CEA algorithm=HmacSHA256, access-key="+accesskey+", signed-date="+datetime+", signature="+signature
-#print out the hmac key
 
-# url = "https://api-gateway.coupang.com"+path+"?%s" % query
-url = "https://api-gateway.coupang.com"+path
-print(url)
+url = "https://api-gateway.coupang.com"+path+"?%s" % query
 
 req = urllib.request.Request(url)
-
 req.add_header("Content-type","application/json;charset=UTF-8")
 req.add_header("Authorization",authorization)
-req.add_header("X-EXTENDED-TIMEOUT", "90000")
+req.add_header("X-EXTENDED-TIMEOUT", "20000")
 
 req.get_method = lambda: method
 
